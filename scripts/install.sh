4611fd48ed7c3114c433803d7d49121f1f9f717e
Dmitri <dz@stackstorm.com>
2018-04-25 22:32:01 -0700

    Test and works

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,10 +11,19 @@ wget -O /vagrant/downloads/root.ca.pem http://www.symantec.com/content/en/us/ent
 # Copy greengrass binaries
 sudo tar -xzf /vagrant/downloads/greengrass-ubuntu-x86-64-1.5.0.tar.gz -C /
 
+# Install ML inference dependencies (MXNet)
+sudo apt-get install -y python-pip python-dev gcc
+sudo pip install mxnet
+sudo apt-get install -y python-opencv
+
 # Copy certificates and configurations
 sudo cp /vagrant/certs/* /greengrass/certs
 sudo cp /vagrant/config/* /greengrass/config
 sudo cp /vagrant/downloads/root.ca.pem /greengrass/certs
+
+# Create a source for images to map into the Lambda space later
+sudo mkdir /images
+sudo chown ggc_user:ggc_group /images
 
 # Back up group.json - you'll thank me later
 sudo cp /greengrass/ggc/deployment/group/group.json /greengrass/ggc/deployment/group/group.json.orig
